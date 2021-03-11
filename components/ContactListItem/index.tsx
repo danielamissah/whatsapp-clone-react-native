946f365d9dc231aa1a23b4522895e2038982cb52
danielamissah <daniel.amissah@congressmail.com>
2021-03-11 01:04:22 +0000

    Create Chat Room

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -4,7 +4,9 @@ import { useNavigation } from '@react-navigation/native';
 import { User } from '../../types';
 import styles from './style';
 import Chats from '../../data/Chats';
-import moment from 'moment';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { createChatRoom, createChatRoomUser } from '../../src/graphql/mutations';
+import ChatRooms from '../../data/ChatRooms';
 
 export type ContactListItemProps ={
     user: User;
@@ -16,11 +18,54 @@ const ContactListItem = (props : ContactListItemProps) => {
 
     const navigation = useNavigation();
 
-    const onClick = () => {
-        navigation.navigate('ChatRoom', 
-        { 
-            //navigae to chat screen with selected user
-        });
+    const onClick = async () => {
+        try {
+            //1. Create a new chat room
+            const newChatRoomData = await API.graphql(
+                graphqlOperation(
+                    createChatRoom, {
+                        input: { }
+                    }
+                )
+            )
+
+            if(!newChatRoomData.data){
+                return;
+            }
+
+            const newChatRoom = newChatRoomData.data.createChatRoom;
+
+            //2. Add 'user' to chat room
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                        input:{
+                            userID: user.id,
+                        chatRoomID: newChatRoom.id,
+                    }
+                        }                        
+                )
+            )
+            //3. Add Authenticated user to the chat room
+            const userInfo = await Auth.currentAuthenticatedUser();
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                        input:{
+                            userID: userInfo.attributes.sub,
+                            chatRoomID: newChatRoom.id,
+                        }
+                        
+                    }
+                )
+            )
+            navigation.navigate('ChatRoom', {
+                id: newChatRoom.id,
+                name: "Hard coded name"
+            })
+        } catch (e) {
+            
+        }
 
     }
 
